94565d8af2afe260a71f4e1f8a48e7363cda9e3a
Jinwoo Hong <73622457+Jinwoo-H@users.noreply.github.com>
2023-07-29 03:13:10 -0400

    Update autotypr.py
    removed unused imports and added relative file path for pyautogui screenshot

--- autotypr.py
+++ autotypr.py
@@ -1,20 +1,15 @@
 import pytesseract
 import PIL.Image
-import cv2
 from pyautogui import *
 import pyautogui
 import time
-import keyboard
-import random
-import win32api, win32con
-
 
 myconfig = r"--psm 6 --oem 3"
 
 time.sleep(3.0)
 
 iml = pyautogui.screenshot(region=(350,460,1200,150))
-iml.save(r"C:\Users\jinwo\Documents\PythonBot\textbox.png")
+iml.save(r"./textbox.png")
 
 text = pytesseract.image_to_string(PIL.Image.open("textbox.png"), config=myconfig)
 text = text.replace("\n", " ")
